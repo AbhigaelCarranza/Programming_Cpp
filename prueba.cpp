599bd70d635d427954d4ce99c419fe0705733401
AbhigaelCarranza <abhicarranz@gmail.com>
2022-03-30 18:58:06 -0700

    Los Valores de a y b fueron agregados

--- prueba.cpp
+++ prueba.cpp
@@ -6,5 +6,7 @@ int main()
     int a=5,b=4;
     int c=a+b;
     cout << "El valor de c es: "<<c<<endl;
+    cout << "El valor de a es: "<<a<<endl;
+    cout << "El valor de b es: "<<b<<endl;
     return 0;
 }
